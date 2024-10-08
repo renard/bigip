89359121decec0f5089226f9745bc80223cac237
Sébastien Gross <sebchezwamorg>
2024-10-08 22:01:06 +0200

    Parser: Add TrafficMatchingCriteria
    TrafficMatchingCriteria is now a supported option in virtual
    configuration.
    
    Signed-off-by: Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>

--- f5/ltm-virtual.go
+++ f5/ltm-virtual.go
@@ -1,7 +1,7 @@
 // Copyright © 2023 Sébastien Gross <seb•ɑƬ•chezwam•ɖɵʈ•org>
 //
 // Created: 2021-12-19
-// Last changed: 2023-07-22 02:58:55
+// Last changed: 2024-10-08 22:00:18
 //
 // This program is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public License
@@ -53,6 +53,7 @@ type LtmVirtual struct {
 	Source                     string                                ` | "source" @Ident`
 	SourcePort                 string                                ` | "source-port" @Ident`
 	SourceAddressTranslation   []*LtmVirtualSourceAddressTranslation ` | "source-address-translation" "{" @@ "}"`
+	TrafficMatchingCriteria    string                                ` | "traffic-matching-criteria"  @(Ident | QF5Name | F5Name | QString)`
 	Translate                  string                                ` | "translate" "service" @( "enable" | "disable")` // OLD device
 	TranslateAddress           string                                ` | "translate-address" @( "enabled" | "disabled")`
 	TranslatePort              string                                ` | "translate-port" @( "enabled" | "disabled")`
